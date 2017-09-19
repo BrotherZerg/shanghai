88b40ada6dcfd3d4c9a6f6d9bc816223591acd56
BrotherZerg <32111274+BrotherZerg@users.noreply.github.com>
2017-09-19 18:19:04 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Team 7, CSC 174 Assignment 4</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">						
+	</head>
+
+	<body>		
+		<header class="page-header">
+			<h1>
+				<a href="index.php">
+					<img src="images/logo.jpg" alt="Logo"></a>
+						<a class="logo" href="index.php"><strong> Web Developers</strong></a>
+			</h1>
+				<p>CSC 174 - Advanced Front-End Web Development</p>
+						
+			<nav>
+				<ul>
+					<li><a class="active" href="index.php">Home</a></li>
+					<li><a href="phoinguyen.php">Phoi Nguyen</a></li>
+					<li><a href="lunguyen.php">Lu Nguyen</a></li>
+				</ul>
+			</nav>			
+		</header>
+
+		<section>
+
+			<header>
+				<h3> Team 7 </h3>
+				<p>
+					Phoi Nguyen - Design Artist <br>  
+					Lu Nguyen -  Information Architect / Code Monkey 
+				</p>
+				
+			</header>
+
+			<article>
+				<h2><em>Phoi Nguyen - Design Artist</em></h2>
+				<figure>
+				<img class="image" src="images/girl.jpg" alt="girl">
+					<figcaption>Design Artist</figcaption>
+				</figure>
+
+				<p>Phoi Nguyen is currently a senior at the University of Rochester, Class of 2018.</p>
+
+				<div class="read"><a href="phoinguyen.php"> Read more...</a></div>
+			</article>
+				
+			<article>
+				<h2><em>Lu Nguyen - Information Architect</em></h2>
+				<figure>
+				<img class="image" src="images/boy.jpg" alt="boy">
+					<figcaption>Information Architect</figcaption>
+				</figure>
+
+				<p>Lu Nguyen is currently a junior at the University of Rochester, Class of 2019.</p>
+
+				<div class="read"><a href="lunguyen.php"> Read more... </a></div>	
+			</article>
+
+			<article>
+				<h2><em>Adam Struhl - Code Monkey</em></h2>
+				<figure>
+				<img class="image" src="images/boy2.png" alt="boy">
+					<figcaption>Code Monkey</figcaption>
+				</figure>
+
+				<p>Lu Nguyen is currently a senior at the University of Rochester, Class of 2018.</p>
+
+				<div class="read"><a href="adam.php"> Read more... </a></div>	
+			</article>
+
+		
+		</section>
+	
+
+		<footer class="footer">		
+				Copyright &copy; 2017	
+				- This webpage is created by Team 7
+		</footer>
+	</body>
+	
+</html>
---
+++ lunguyen.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Team 7, CSC 174 Assignment 4</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	
+								
+	</head>
+
+	<body>		
+		<header class="page-header">
+			<h1>
+				<a href="index.php">
+					<img src="images/logo.jpg" alt="Logo"></a>
+						<a class="logo" href="index.php"><strong> Web Developers</strong></a>
+			</h1>	
+				<p>CSC 174 - Advanced Front-End Web Development</p>
+						
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="phoinguyen.php">Phoi Nguyen</a></li>
+					<li><a class="active" href="lunguyen.php">Lu Nguyen</a></li>
+				</ul>
+			</nav>					
+		</header>
+
+		<section>
+		<h4>Lu Nguyen</h4>
+
+		<h2>Education</h2>
+
+			<p>I am currently a junior at the University of Rochester, Class of 2019. I am majoring in <strong>Digitial Media Studies</strong> and minoring in <em>Computer Sciences</em>. I love to design everything like objects, graphics, or website. I am transfer student after graduating past college as knowns as Monroe Community College with Mathematics degree. But I have changed my major to DMS because I think designing is approriately best suit for me. This is a reason why I am taking CSC 174 course. I had planed to earn Bachelor degree in University of Rochester and Master degree in Rochester Institute of Technology.</p>
+
+		<h2>Work Experiences</h2>
+
+			<p>I have been working as Technical Assisstant in Interfaith Chapel at University of Rochester. Moreover, I had a good job as tutor and student help desk in Monroe Community College for 2 years.m</p>
+
+		<h3>Clubs and Organization</h3>
+
+			<p>I was an 2017 Orientation Volunteer for class of 2021. I'm joining these clubs: </p>
+			<ul style="list-style-type: square;">
+				<li>Circle K International</li>
+				<li>Modern Languages and Cultures Undergraduate Council</li>
+				<li>Society of Hispanic Professional Engineers</li>
+				<li>Student's Association of Interfaith Cooperation</li>
+				<li>Undergraduate Film Council</li>
+				<li>UR eSports</li>
+			</ul>
+		</section>	
+			<?php include "myinclude.inc" ?>			
+	
+
+		<footer>		
+				Copyright &copy; 2017	
+				- This webpage is created by Team 7
+		</footer>
+	</body>
+</html>
+
