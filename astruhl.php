826606d604d922c60ad5120fe985dbeed45eb358
dragon1472 <dragon1472.0@gmail.com>
2017-09-21 02:03:16 -0400

    Add files via upload

---
+++ astruhl.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Adam Struhl</title>
+	</head>
+	
+	<body>
+	
+	<h1>Adam Struhl</h1>
+	
+	<h2>About</h2>
+
+
+	<?php include "inc/footer.inc"; ?>
+
+	
+	<p>
+		Finding himself in a position to be the writer of his own autobiography, Adam decided to do the responsible thing with this assignment and not exaggerate his achievements.  Like how he's totally the 5 time Worlds Best Ultra Extreme Mega Hoverbike Champion or some other stupid thing.  
+	</p>
+	<p>
+		But if you really want to know about me as a person, then you're probably going to want to hear about how I'm a creative type dedicated to pursuing new and innovative ways to save the future, improve technology in great strides, and some other third DMS related pipe dream.  Which is what everyone says that they are going to do.  But while it is true that I am a bit of a creative person, my real passion is for story telling.  I'm a dual Major, pursuing Creative writing and DMS, and while I do enjoy doing front end work and intend to find employment doing it, my dream is to be able to create worlds and characters that stir the imagination of their readers.  I know that it's a hard dream, considering that state of the literary world in this digital age, but I do believe that there is a place in the world for good stories.
+	</p>
+
+	<h2>Actual Personal Skills Stuff</h2>
+	
+	<p>
+		In terms of actual computer skills I am pretty good with the usual DMS repertoire of HTML, CSS, PHP, and Java code.  I am also a pretty proficient Photoshop and Illustrator user, since I can hack togetehr anything I'd really need for the visual aspects of a website all on my own.  I am a bit weaker with Java though, so it does take me a bit longer to code things in it.
+	</p>
+
+	</body>
+	</html>
