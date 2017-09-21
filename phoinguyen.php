86d8e723566bf7b7d5efa7cd55c0cd86545602b8
BrotherZerg <32111274+BrotherZerg@users.noreply.github.com>
2017-09-21 13:38:16 -0400

    Update phoinguyen.php

--- phoinguyen.php
+++ phoinguyen.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Team 7, CSC 174 Assignment 4</title>
+		<title>Team Shanghai, CSC 174 Assignment 4</title>
 		<link rel="stylesheet" type="text/css" href="css/styles.css">						
 	</head>
 
