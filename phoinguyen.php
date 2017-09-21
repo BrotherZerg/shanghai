9fbb61986d5114f136b388ba1f89650e164e1239
BrotherZerg <32111274+BrotherZerg@users.noreply.github.com>
2017-09-21 19:26:47 -0400

    Add files via upload

---
+++ phoinguyen.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<?php include "head.inc" ?>			
+
+	<body>		
+		<header class="page-header">
+			<h1>
+				<a href="index.php">
+					<img src="images/logo.jpg" alt="Logo"></a>
+						<a class="logo" href="index.php"><strong> Web Developers</strong></a>
+			</h1>	
+				<p>CSC 174 - Advanced Front-End Web Development</p>
+						
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a class="active" href="phoinguyen.php">Phoi Nguyen</a></li>
+					<li><a href="lunguyen.php">Lu Nguyen</a></li>
+					<li><a href="astruhl.php">Adam Struhl</a></li>
+				</ul>
+			</nav>			
+		</header>
+
+		<section>
+			<h4>Phoi Nguyen</h4>
+
+			<h2>Education</h2>
+
+			<p>I'm currently a senior at the University of Rochester, Class of 2018. I'm majoring in <strong>Mathematics</strong> and minoring in <em>Statistics</em>. I'm really fond of creating and desgning websites. That's a reason why I'm taking this CSC 174 class. I plan to attend a gratuate school to earn a master degree related to computer and information technology program. </p>
+
+			<h2>Work Experiences</h2>
+
+			<p>I have been working as office assistant at the Uinversity of Rochester Admissions Office for 2 years. Simultaneously, I'm also working as event assistant at the Event and Classroom Management Department in U of R. For my future career, I will be seeking a postion in technical development.</p>
+
+
+			<h2>Clubs and Organization</h2>
+
+				<p>I'm joining school clubs to enhance and develop my social network. Here is the list that I'm an active member:</p>
+				<ul style="list-style-type:square">
+					<li>Society of Undergraduate Mathematics Students (SUMS)</li>
+					<li>Circle K International (CKI)</li>
+					<li>Society of Hispanic Professional Engineers (SHPE)</li>
+					<li>Students' Association for Interfaith Cooperation (SAIC) </li>
+					<li>Community Service Network (CSN)</li>
+				</ul>
+		</section>
+			
+		<?php include "footer.inc" ?>			
+
+	</body>
+</html>
